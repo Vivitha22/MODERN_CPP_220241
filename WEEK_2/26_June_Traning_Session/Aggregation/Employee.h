e9af8577a21be28aa0cdf6737a89b86399f8d8d5
Vivitha V <Vivitha.V@kpit.com>
2024-06-27 15:06:46 +0530

    jh

--- WEEK_2/26_June_Traning_Session/Aggregation/Employee.h
+++ WEEK_2/26_June_Traning_Session/Aggregation/Employee.h
@@ -17,6 +17,18 @@ private:
 public:
     Employee(int id, std::string name, ProjectRef pr);
     ~Employee();
+
+    ProjectRef project() const { return _project; }
+
+
+    friend std::ostream &operator<<(std::ostream &os, const Employee &rhs) {
+        os << "_id: " << rhs._id
+           << " _name: " << rhs._name
+           << " _project: " << rhs._project;
+        return os;
+    }
+
+    
 };
 
 #endif // EMPLOYEE_H
